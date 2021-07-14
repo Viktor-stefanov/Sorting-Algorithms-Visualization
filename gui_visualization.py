35e6dad0a3ff6dbd0579cdd85dc6d35e635d736b
Viktor <58659727+Viktor-stefanov@users.noreply.github.com>
2021-07-14 08:27:44 -0700

    finished app

--- gui_visualization.py
+++ gui_visualization.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from sorting_algorithms.sorts import *
-from info import *
+from info import sorts
 from tkinter import ttk
-from tkinter import font
 
 def round_rectangle(x1, y1, x2, y2, canvas, radius=25, **kwargs):
+    """ create a round rectangle with the create_polygon method of the canvas tkinter object. P.S. Tkinter sucks! """
     points = [x1 + radius, y1,
               x1 + radius, y1,
               x2 - radius, y1,
@@ -38,151 +38,187 @@ def clear_screen(root):
     for child in root.winfo_children():
         child.destroy()
 
-def main_screen(root=None):
-    """ set up the main window, it's dimensions, title, icon, etc...
-        set up the header and 'choose box' widgets and display them on the screen """
-
-    def on_start_click():
-        # handle pressing the start button
-        start_canvas.configure(relief="raised")
-        on_start(root, sorting_alg.get(), array_size.get(), sorting_speed.get())
-
-    # this check is made in case we want to 'redraw' the screen without actually opening a new 'tab' (another root instance)
-    if root is None:
+class Visualization:
+    def __init__(self):
+        """ set up the main window, it's dimensions, title, icon, etc... """
         icon = ""
-        root = tkinter.Tk()
-        root.option_add("*TCombobox*Listbox.font", ("Aria;", 18))
-        root.title("Sorting Algorithms Visualization")
-        # root.iconbitmap(icon)
-        root.geometry("1400x900")
-        root.configure(background="#8ecae6")
-    else: # if root is passed clean the current screen before creating the 'main' screen
-        clear_screen(root)
-    root.bind("<Key-q>", sys.exit)
-    root.bind("<Key-m>", lambda e: main_screen(root))
+        self.root = tkinter.Tk()
+        self.root.option_add("*TCombobox*Listbox.font", ("Aria;", 18))
+        self.root.title("Sorting Algorithms Visualization")
+        # self.root.iconbitmap(icon)
+        self.root.geometry("1400x900")
+        self.root.configure(background="#8ecae6")
+        self.root.bind("<Key-q>", sys.exit)
+        self.root.bind("<Key-m>", lambda e: self.main_screen())
+        # initialize variables
+        self.combobox_v = None  # tkinter.StringVar() to inform us when the user has selected an algorithm from the combobox
+        self.alg_information = None  # the short description which shows when you select an algorithm in the combobox
+        self.sorting_alg = None  # which algorithm has been chosen
+        self.array_size = None  # the size of the to be sorted array
+        self.sorting_speed = None  # the speed of the animation
+        self.graph_canvas = None  # FigureCanvasTkAgg
+        self.operation_var = None  # operation counter StringVar()
+        self.operations = 0  # operations counter for the graph
 
-    header = tkinter.Label(text="Sorting Algorithms Visualization", font=("Aria;",  48, "bold"), background="#023047", fg="white")
-    alg_canvas = tkinter.Canvas(root, bg="#8ecae6", highlightthickness=0)
-    round_rectangle(0, 0, 425, 120, alg_canvas, radius=25, fill="#a2d2ff")
-    algorithms = ("Bubble Sort", "Selection Sort", "Insertion Sort", "Merge Sort", "Quick Sort", "Bogo Sort")
-    sorting_alg_label = tkinter.Label(text="Sorting algorithm:", font=("Aria;", 28), bg="#a2d2ff")
-    sorting_alg = ttk.Combobox(values=algorithms, font=("Aria;", 24), state="readonly", width=20)
-    sorting_alg.current(0) # set the current item to the first item
-    header.pack(fill=tkinter.X)
-    alg_canvas.place(x=65, y=130, w=425)
-    sorting_alg_label.place(x=75, y=140)
-    sorting_alg.place(x=75, y=195)
+        self.main_screen()
 
-    size_canvas = tkinter.Canvas(root, bg="#8ecae6", highlightthickness=0)
-    round_rectangle(0, 0, 325, 120, size_canvas, fill="#a2d2ff")
-    array_size_label = tkinter.Label(text="Array size:", font=("Aria;", 28), bg="#a2d2ff")
-    array_size = ttk.Combobox(values=("Small", "Average", "Big"), state="readonly", font=("Aria;", 24), width=11)
-    array_size.current(0)  # set the current item to the first item
-    size_canvas.place(x=540, y=130, w=425)
-    array_size_label.place(x=590, y=140)
-    array_size.place(x=590, y=195)
+    def main_screen(self):
+        """ create the 'main menu' layout and draw it on the screen"""
+        self.operations = 0
+        def on_start_click():
+            # when the user clicks the start button activate the on_start function to continue
+            self.on_start()
 
-    speed_canvas = tkinter.Canvas(root, bg="#8ecae6", highlightthickness=0)
-    round_rectangle(0, 0, 425, 120, speed_canvas, fill="#a2d2ff")
-    sorting_speed_label = tkinter.Label(text="Sorting speed:", font=("Aria;", 28), bg="#a2d2ff")
-    sorting_speed = ttk.Combobox(values=("Study", "Slow", "Fast"), state="readonly", font=("Aria;", 24), width=15)
-    sorting_speed.current(0)  # set the current item to the first item
-    speed_canvas.place(x=915, y=130, w=425)
-    sorting_speed_label.place(x=975, y=140)
-    sorting_speed.place(x=975, y=195)
+        def alg_selected(index, value, op):
+            # show a brief description when a sorting alg has been selected from the combobox
+            information = [sort for sort in sorts if self.combobox_v.get().lower() in sort.lower()][
+                0]  # if eg 'Bubble Sort' is in the sort description then choose that one. There are no duplicates of let's say 2 'Bubble Sort' in 2 descriptions
+            self.alg_information.config(text=information)
 
-    # use a canvas as a 'button' and set up event handlers for click press and release
-    start_canvas = tkinter.Canvas(root, bg="#8ecae6", highlightthickness=0)
-    round_rectangle(0, 0, 170, 50, start_canvas, fill="#a2d2ff")
-    start_canvas.create_text(85, 25, fill="black", font=("Aria;", 20, "bold"), text="Visualize")
-    start_canvas.bind("<ButtonRelease-1>", lambda e: on_start_click())
-    start_canvas.place(x=1200, y=825, w=200, h=50)
+        clear_screen(self.root) # clean the screen from other layouts
 
-    root.mainloop()
+        header = tkinter.Label(text="Sorting Algorithms Visualization", font=("Aria;",  48, "bold"), background="#023047", fg="white")
+        alg_canvas = tkinter.Canvas(self.root, bg="#8ecae6", highlightthickness=0)
+        round_rectangle(0, 0, 425, 120, alg_canvas, radius=25, fill="#a2d2ff")
+        algorithms = ("Bubble Sort", "Selection Sort", "Insertion Sort", "Merge Sort", "Quick Sort", "Bogo Sort")
+        self.combobox_v = tkinter.StringVar()
+        self.combobox_v.trace("w", alg_selected)
+        sorting_alg_label = tkinter.Label(self.root, text="Sorting algorithm:", font=("Aria;", 28), bg="#a2d2ff")
+        self.sorting_alg = ttk.Combobox(self.root, values=algorithms, textvar=self.combobox_v, font=("Aria;", 24), state="readonly", width=16)
+        header.pack(fill=tkinter.X)
+        alg_canvas.place(x=65, y=130, w=425)
+        sorting_alg_label.place(x=126, y=140)
+        self.sorting_alg.place(x=120, y=195)
 
-def on_start(root, alg, size, speed):
-    """ helper function to clean the screen and build the graph, array, set speed, etc... before graphing of the algorithm """
-    clear_screen(root)
-    # check the function name e.g. (bogo_sort) with the current algorithm's name and if they match you assign the function to the algorithm variable
-    algorithm = [sort for sort in (bubble_sort, insertion_sort, selection_sort, merge_sort, quick_sort, bogo_sort) if sort.__name__.replace("_", " ") == alg.lower()][0]
-    array_size = 25 if size == "Small" else 100 if size == "Average" else 200
-    sort_speed = speed if speed == "Study" else 500 if speed == "Slow" else 1
-    # initialize an array of size - array_size with random numbers from 1 to the size of the array and get the iterator object of the sorting alg chosen
-    arr = random.sample(range(1, array_size+1), array_size)
-    generator = algorithm(arr)
+        information_canvas = tkinter.Canvas(self.root, bg="#8ecae6", highlightthickness=0, width=2000, height=1000)
+        round_rectangle(0, 0, 1255, 380, information_canvas, radius=25, fill="#a2d2ff")
+        self.alg_information = tkinter.Label(information_canvas, text="", font=("Aria;", 28), wraplength=1240, justify="left", bg="#a2d2ff")
+        information_canvas.place(x=75, y=325)
+        self.alg_information.place(x=15, y=15)
+        self.sorting_alg.current(0)  # set the algorithm combobox default to bubble sort. This will also trigger the alg_selected function since we are tracing the self.combobox_v StringVar() and will put the description of bubblesort on the screen
 
-    fig, ax, canvas, str_var = graph_layout(root, array_size, alg)
-    start_visualization(generator, alg, sort_speed, fig, ax, canvas, str_var)
+        size_canvas = tkinter.Canvas(self.root, bg="#8ecae6", highlightthickness=0, height=150)
+        round_rectangle(0, 0, 325, 120, size_canvas, fill="#a2d2ff")
+        array_size_label = tkinter.Label(text="Array size:", font=("Aria;", 28), bg="#a2d2ff")
+        self.array_size = ttk.Combobox(values=("Miniature", "Small", "Average", "Big"), state="readonly", font=("Aria;", 24), width=10)
+        self.array_size.current(0)  # set the current item to the first item
+        size_canvas.place(x=540, y=130, w=425)
+        array_size_label.place(x=615, y=140)
+        self.array_size.place(x=600, y=195)
 
-def graph_layout(root, array_size, alg):
-    """ layout of the APP while the graph is being visualized. Returns the ax object for later use in the bar chart """
-    # create the figure, axes etc... (set up the graph)
-    fig = plt.Figure(figsize=(15, 8), dpi=100, facecolor="#8ecae6")
-    ax = fig.add_subplot(1, 1, 1)
-    ax.tick_params(axis="x", colors="white") # change the tick colors to white on both the x and the y axis
-    ax.tick_params(axis="y", colors="white")
-    # create the embedded canvas object and an operation counter
-    canvas = FigureCanvasTkAgg(fig, master=root)
-    string_var = tkinter.StringVar()
-    string_var.set(f"Operations: 0")
-    op_counter = tkinter.Label(root, textvariable=string_var, font=("Aria;", 18, "bold"), bg="#8ecae6", fg="#a2d2ff")
-    # create a 'header' using a canvas for the round edges...
-    header_canvas = tkinter.Canvas(root, bg="#8ecae6", highlightthickness=0, height=110)
-    round_rectangle(0, 0, 1200, 60, header_canvas, fill="#a2d2ff")
-    header = tkinter.Label(root, text=f"{alg.capitalize()} performed on an array of {array_size} elements", font=("Aria;", 28, "bold"), bg="#a2d2ff")
-    # use a canvas as a menu button
-    menu_canvas = tkinter.Canvas(root, bg="#8ecae6", highlightthickness=0)
-    round_rectangle(0, 0, 170, 50, menu_canvas, fill="#a2d2ff")
-    menu_canvas.create_text(85, 25, fill="black", font=("Aria;", 20, "bold"), text="Menu")
-    menu_canvas.bind("<ButtonRelease-1>", lambda e: main_screen(root))
-    # put everything on the screen
-    header_canvas.pack(fill=tkinter.BOTH, padx=(100, 0), pady=(60, 0))
-    menu_canvas.place(x=1200, y=825, w=200, h=50)
-    canvas.get_tk_widget().place(x=-67, y=60)
-    header.place(x=120, y=69)
-    op_counter.place(x=120, y=820)
+        speed_canvas = tkinter.Canvas(self.root, bg="#8ecae6", highlightthickness=0, height=180)
+        round_rectangle(0, 0, 425, 120, speed_canvas, fill="#a2d2ff")
+        sorting_speed_label = tkinter.Label(text="Sorting speed:", font=("Aria;", 28), bg="#a2d2ff")
+        self.sorting_speed = ttk.Combobox(values=("Study", "Slow", "Fast"), state="readonly", font=("Aria;", 24), width=13)
+        self.sorting_speed.current(0)  # set the current item to the first item
+        speed_canvas.place(x=915, y=130, w=425)
+        sorting_speed_label.place(x=990, y=140)
+        self.sorting_speed.place(x=985, y=195)
 
-    return fig, ax, canvas, string_var
+        # use a canvas as a 'button' and set up event handlers for click press and release
+        start_canvas = tkinter.Canvas(self.root, bg="#8ecae6", highlightthickness=0)
+        round_rectangle(0, 0, 170, 50, start_canvas, fill="#a2d2ff")
+        start_canvas.create_text(85, 25, fill="black", font=("Aria;", 20, "bold"), text="Visualize")
+        start_canvas.bind("<ButtonRelease-1>", lambda e: on_start_click())
+        start_canvas.place(x=1200, y=825, w=200, h=50)
 
-def start_visualization(generator, alg_name, sort_speed, fig, ax, canvas, str_var):
-    """ method does the actual graphing of the sorting algorithm """
-    operations = 0
-    def simple_frame(frames):
-        """ function is called every time a frame is being visualized
-            change the position of the bars according to the current 'yield' from the sorting algorithm
-            difference between simple & complex is in the unpacking of the 'frames' values (bubble, insertion, selection - 3 items, all other 4, merge 5)"""
-        nonlocal operations
-        arr, left, right = frames
-        for index, bar in enumerate(bars):
-            bar.set_height(arr[index])
-            bar.set_color("paleturquoise")
-            if index in (left, right):
-                bar.set_color("r")
-            bar.set_edgecolor((0, 0, 0))
-        operations += 1
-        str_var.set(f"Operations: {operations}")
-        fig.canvas.draw()
-        canvas.flush_events()
+        self.root.mainloop()
 
-    # def complex_frame(frames):
-    #     nonlocal operations
-    #     arr, left, right
+    def on_start(self):
+        """ helper function to clean the screen and build the graph, array, sort speed, etc... before graphing the algorithm """
+        alg_name = self.sorting_alg.get()
+        size = self.array_size.get()
+        speed = self.sorting_speed.get()
+        clear_screen(self.root)
+        # check the function name e.g. (bogo_sort) with the current algorithm's name and if they match you assign the function to the algorithm variable
+        self.sorting_alg = [sort for sort in (bubble_sort, insertion_sort, selection_sort, merge_sort, quick_sort, bogo_sort) if sort.__name__.replace("_", " ") == alg_name.lower()][0]
+        self.array_size = 10 if size == "Miniature" else 25 if size == "Small" else 100 if size == "Average" else 200
+        self.sorting_speed = speed if speed == "Study" else 500 if speed == "Slow" else 1
+        if self.sorting_alg == bogo_sort:
+            self.array_size = 5 if size == "Miniature" else 6 if size == "Small" else 8 if size == "Average" else 10
+        # initialize an array of size self.array_size with random numbers from 1 to the size of the array and get the generator object of the sorting alg chosen
+        arr = random.sample(range(1, self.array_size+1), self.array_size)
+        if alg_name in ("Quick Sort", "Merge Sort"):
+            generator = self.sorting_alg(arr, 0, len(arr)-1)
+        else:
+            generator = self.sorting_alg(arr)
 
+        fig, ax = self.graph_layout(alg_name)
+        self.start_visualization(generator, arr, fig, ax)
 
-    # get the first iteration of the sorting algorithm to create a bars object (which we manipulate in the visualize_frame function above
-    array, left_item, right_item = next(generator)
-    bars = ax.bar(sorted(array), array)
-    # create the animation. frames argument takes a generator and passes it's yield statements as arguments to visualize_frame. That is why we unpack 3 things in the function: arr, left, right = frames
-    if sort_speed != "Study":
-        ani = animation.FuncAnimation(
-            fig,
-            simple_frame,
-            frames=generator,
-            interval=sort_speed,
-            repeat=False
-        )
-        ani._start() # start the animation
+    def graph_layout(self, alg_name):
+        """ create the layout of the app displayed while the graph will be visualized """
+        # create the figure, axes etc... (set up the graph)
+        fig = plt.Figure(figsize=(15, 8), dpi=100, facecolor="#8ecae6")
+        ax = fig.add_subplot(1, 1, 1)
+        ax.tick_params(axis="x", colors="white") # change the tick colors to white on both the x and the y axis
+        ax.tick_params(axis="y", colors="white")
+        # create the embedded canvas object and an operation counter
+        self.graph_canvas = FigureCanvasTkAgg(fig, master=self.root)
+        self.operation_var = tkinter.StringVar()
+        self.operation_var.set(f"Operations: 0")
+        op_counter = tkinter.Label(self.root, textvariable=self.operation_var, font=("Aria;", 18, "bold"), bg="#8ecae6", fg="#a2d2ff")
+        # create a 'header' using a canvas for the round edges...
+        header_canvas = tkinter.Canvas(self.root, bg="#8ecae6", highlightthickness=0, height=110)
+        round_rectangle(0, 0, 1200, 60, header_canvas, fill="#a2d2ff")
+        header = tkinter.Label(self.root, text=f"{alg_name.capitalize()} performed on an array of {self.array_size} elements", font=("Aria;", 28, "bold"), bg="#a2d2ff")
+        # use a canvas as a menu button
+        menu_canvas = tkinter.Canvas(self.root, bg="#8ecae6", highlightthickness=0)
+        round_rectangle(0, 0, 170, 50, menu_canvas, fill="#a2d2ff")
+        menu_canvas.create_text(85, 25, fill="black", font=("Aria;", 20, "bold"), text="Menu")
+        menu_canvas.bind("<ButtonRelease-1>", lambda e: self.main_screen())
+        # put everything on the screen
+        header_canvas.pack(fill=tkinter.BOTH, padx=(100, 0), pady=(60, 0))
+        menu_canvas.place(x=1200, y=825, w=200, h=50)
+        self.graph_canvas.get_tk_widget().place(x=-67, y=60)
+        header.place(x=120, y=69)
+        op_counter.place(x=120, y=820)
+
+        return fig, ax
+
+    def start_visualization(self, generator, array, fig, ax):
+        """ method does the actual graphing of the sorting algorithm """
+        def study_frame(event):
+            try:
+                next_frame = next(generator)
+                display_frame(next_frame, study=True)
+            except StopIteration:
+                print("finished")
+                # call self.finished_visualization(). And create it ofc
+
+        def display_frame(frame, study=False):
+            """ display individual frame from the animation. Set the height of the bars, their colors and update the operations counter """
+            arr, low, mid, high = frame  # again tkinter stuff...
+            for index, bar in enumerate(bars):
+                bar.set_height(arr[index])
+                bar.set_color("paleturquoise")
+                if index == high:
+                    bar.set_color("r")
+                elif index == low:
+                    bar.set_color("g")
+                if index in mid:
+                    bar.set_color("#000000")
+                bar.set_edgecolor((0, 0, 0))
+            self.operations += 1
+            self.operation_var.set(f"Operations: {self.operations}")
+            if study is True:
+                fig.canvas.draw()
+
+        bars = ax.bar(sorted(array), array)
+
+        # create the animation. frames argument takes a generator and passes it's yield statements as arguments to visualize_frame. That is why we unpack 3 things in the function: arr, left, right = frames
+        if self.sorting_speed != "Study":
+            ani = animation.FuncAnimation(
+                fig,
+                display_frame,
+                frames=generator,
+                interval=self.sorting_speed,
+                repeat=False,
+            )
+            ani._start()  # start the animation
+        else:  # if the user chose the 'study" sorting speed show frames only when the user presses the mouse
+            self.root.bind("<ButtonPress>", study_frame)
 
 
 if __name__ == "__main__":
-    main_screen()
+    app = Visualization()
--- sorting_algorithms/sorts.py
+++ sorting_algorithms/sorts.py
@@ -3,38 +3,38 @@ from random import shuffle
 def swap(arr, i, j):
     arr[i], arr[j] = arr[j], arr[i]
 
-def bubblesort(arr):
+def bubble_sort(arr):
     swapped = False
     for i in range(len(arr) - 1):
         for j in range(1, len(arr) - i):
             if arr[j-1] > arr[j]:
                 swapped = True
                 swap(arr, j, j-1)
-                yield arr, j, j-1
+                yield arr, j, (None, ), j-1
             else:
-                yield arr, j-1, j
+                yield arr, j-1, (None, ), j
         if not swapped:
             break
 
-def selectionsort(arr):
+def selection_sort(arr):
     for i in range(len(arr)-1):
         smallest = i
         for j in range(i + 1, len(arr)):
             if arr[j] < arr[smallest]:
                 smallest = j
-            yield arr, smallest, j
+            yield arr, smallest, (None, ), j
         swap(arr, i, smallest)
 
-def insertionsort(arr):
+def insertion_sort(arr):
     for i in range(1, len(arr)):
         insert_item = arr[i]
         j = i - 1
-        while j >= 0 and insert_item < arr[j]:
+        while j >= 0 and insert_item <= arr[j]:
             arr[j + 1] = arr[j]
             j -= 1
-            yield arr, i, j
+            yield arr, i, (None, ), j
         arr[j+1] = insert_item
-        yield arr, j + 1, i
+        yield arr, j + 1, (None, ), i
 
 def merge(arr, low, mid, high):
     merged = []
@@ -59,45 +59,44 @@ def merge(arr, low, mid, high):
 
     for ind, value in enumerate(merged):
         arr[low + ind] = value
-        yield arr, high, mid, low + ind
+        yield arr, high, (mid, ), low + ind
 
-def mergesort(arr, low, high):
+def merge_sort(arr, low, high):
     if low >= high:
         return arr
 
     mid = low + ((high - low + 1) // 2) - 1
 
-    yield from mergesort(arr, low, mid)
-    yield from mergesort(arr, mid+1, high)
+    yield from merge_sort(arr, low, mid)
+    yield from merge_sort(arr, mid + 1, high)
 
     yield from merge(arr, low, mid, high)
 
-def quicksort(arr, low, high):
+def quick_sort(arr, low, high):
     if low >= high:
         return arr
 
     pivot = arr[high]
-    mid = (low + high) // 2
-    pivotIdx = low
+    pivot_index = low
     for i in range(low, high):
         if arr[i] < pivot:
-            swap(arr, i, pivotIdx)
-            pivotIdx += 1
-        yield arr, (pivotIdx, i), (low, high)
-    swap(arr, high, pivotIdx)
-    yield arr, (pivotIdx, i), (low, high)
+            swap(arr, i, pivot_index)
+            pivot_index += 1
+        yield arr, low, (pivot_index, i), high
+    swap(arr, high, pivot_index)
+    yield arr, low, (pivot_index, i), high
 
-    yield from quicksort(arr, low, pivotIdx - 1)
-    yield from quicksort(arr, pivotIdx + 1, high)
+    yield from quick_sort(arr, low, pivot_index - 1)
+    yield from quick_sort(arr, pivot_index + 1, high)
 
-def bogosort(arr):
+def bogo_sort(arr):
     flag = True
     while flag:
         for i in range(len(arr) - 1):
             if arr[i] > arr[i + 1]:
-                yield arr
+                yield arr, -1, (None, ), -1
                 shuffle(arr)
                 break
         else:
-            yield arr
+            yield sorted(arr), None, (None, ), None
             flag = False
